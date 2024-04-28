ed69647158cd80b0c0708035be9cf392cd8c39af
Shapovalov Viacheslav <slavko.ukrop@gmail.com>
2024-04-28 17:31:46 +0300

    Update vite.config.ts with server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    // https://github.com/laravel/vite-plugin/issues/28
+    server: {
+        host: "0.0.0.0",
+        hmr: {
+            host: "localhost",
+        },
+    },
+});
